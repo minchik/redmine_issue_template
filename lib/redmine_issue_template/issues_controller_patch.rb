b4956b0253cf0613710e5964b2eca925b989edd0
minchik <minchik@gmail.com>
2025-04-18 13:59:45 +0300

    Add issue templates based on project

--- lib/redmine_issue_template/issues_controller_patch.rb
+++ lib/redmine_issue_template/issues_controller_patch.rb
@@ -7,13 +7,23 @@ module RedmineIssueTemplate
       super
       if @issue&.new_record? && params[:issue].blank?
         plugin = Redmine::Plugin.find(:redmine_issue_template)
-        template_path = File.join(plugin.assets_directory, 'templates', 'default_issue_template.md')
+        project_template_path = File.join(plugin.assets_directory, 'templates', "issue_template_#{@issue.project.identifier}.md")
+        default_template_path = File.join(plugin.assets_directory, 'templates', 'default_issue_template.md')
+        template_path = if File.exist?(project_template_path)
+                          project_template_path
+                        else
+                          default_template_path
+                        end
         if File.exist?(template_path)
           content = File.read(template_path)
           @issue.description = content
-          Rails.logger.info("RedmineIssueTemplate: Applied default template in build_new_issue_from_params.")
+          if template_path == project_template_path
+            Rails.logger.info("RedmineIssueTemplate: Applied project template for project '#{@issue.project.identifier}'.")
+          else
+            Rails.logger.info("RedmineIssueTemplate: Applied default template in build_new_issue_from_params.")
+          end
         else
-          Rails.logger.warn("RedmineIssueTemplate: Template file not found at #{template_path}")
+          Rails.logger.warn("RedmineIssueTemplate: No template file found at #{project_template_path} or #{default_template_path}")
         end
       end
     rescue => e
